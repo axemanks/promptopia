a5c1ba55749259c21cdc721c3d43c910611ae9c4
axemanks <keithscheldt@gmail.com>
2023-05-08 10:58:18 -0400

    corrected user profile bug

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -2,17 +2,16 @@
 
 "use client";
 
-import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const UserProfile = () => {
-    const searchParams = useSearchParams();
-    const userName = searchParams.get("name");
-  
-    const [userPosts, setUserPosts] = useState([]);
+const UserProfile = ({ params }) => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get("name");
+
+  const [userPosts, setUserPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -23,16 +22,13 @@ const UserProfile = () => {
     };
 
     if (params?.id) fetchPosts();
-  }, [params?.id]); // trigger when params.id changes if there is one
-    
-    
-    return (
-        <Profile
-            name={userName}
+  }, [params.id]);
+
+  return (
+    <Profile
+      name={userName}
       desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
       data={userPosts}
-      handleEdit={handleEdit}
-      handleDelete={handleDelete}
     />
   );
 };
